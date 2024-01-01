a830365b1d25b41a3f064e5f6b958d1bd5225d3d
tranphu <tranphudhak@gmail.com>
2024-01-01 22:00:33 +0700

    fix: fix displaying notification content

--- frontend/src/view/candidate/layouts/BellDialog.js
+++ frontend/src/view/candidate/layouts/BellDialog.js
@@ -10,12 +10,12 @@ export default function BellDialog({ show, setShow, current }) {
     <Modal show={show} onHide={handleClose} size="lg">
       <Modal.Body>
         <h4 className="border-bottom border-2 pb-2 text-center">Thông báo</h4>
-        <div className="text-dark ts-ssm">{current.name}</div>
+        <div className="ts-ssm text-danger">{current.name}</div>
         {/* <div className="text-secondary ts-sm">
           Đã nhận lúc {dayjs(current.created_at).format("H:m - DD/MM/YYYY")}
         </div> */}
         <div className="mt-2 ps-3 fw-500 ts-17">{current.title}</div>
-        <div className="mt-1 border border-2 rounded py-1 ps-2 bg-light">
+        <div className="mt-1 border border-2 rounded py-1 ps-2 bg-light whitespace-pre-line">
           {current.content}
         </div>
         <Button
