86ce8127aae4463df544f346f5aef1360447c237
tranphu <tranphudhak@gmail.com>
2024-02-23 19:42:45 +0700

    UI: fix list company page

--- frontend/src/view/candidate/CompanyList.js
+++ frontend/src/view/candidate/CompanyList.js
@@ -1,10 +1,14 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./custom.css";
 import employerApi from "../../api/employer";
 import { AppContext } from "../../App";
+import { IoMdPeople } from "react-icons/io";
+import { MdLocationOn } from "react-icons/md";
+import { IoIosLink } from "react-icons/io";
 
 function CompanyList() {
+  const nav = useNavigate();
   const [companies, setCompanies] = useState([]);
   const [comKey, setComKey] = useState("");
   const { setCurrentPage } = useContext(AppContext);
@@ -31,11 +35,9 @@ function CompanyList() {
   }, [setCurrentPage]);
 
   return (
-    <>
-      {/* <h2 className="p-2 mb-3 shadow bg-light">Danh sách công ty</h2> */}
+    <div style={{ margin: "0px 100px" }}>
       <form
         className="d-flex pt-3"
-        style={{ marginLeft: "75px" }}
         onSubmit={handleSubmit}
       >
         <input
@@ -46,89 +48,78 @@ function CompanyList() {
           placeholder="Tìm tên công ty..."
           onChange={handleChange}
         />
-        <button type="submit" className="ms-1 btn btn-primary">
+        <button type="submit" className="ms-2 btn btn-primary">
           Tìm kiếm
         </button>
       </form>
-      <div className="container-fluid col-md-11">
-        <div className="row mt-3">
-          {companies.length > 0 ? (
-            companies.map((company) => (
+      <div className="row mt-3">
+        {companies.length > 0 ? (
+          companies.map((company) => (
+            <div className="col-sm-12 col-lg-4 mb-3 pointer" key={company.id}>
               <div
-                className="col-md-4 mb-3"
-                key={company.id}
-                style={{ cursor: "pointer" }}
+                className="card border-main-light hover-shadow-sm"
+                style={{ minHeight: "240px" }}
+                onClick={() => nav(`/companies/${company.id}`)}
               >
-                <div className="card abc" style={{ minHeight: "280px" }}>
-                  <div
-                    className="d-flex border-bottom"
-                    style={{ minHeight: "110px" }}
-                  >
-                    <div className="border-end d-flex align-items-center">
-                      <img
-                        src={company.logo}
-                        className=""
-                        style={{ maxWidth: "110px", maxHeight: "110px" }}
-                        alt={company.name}
-                      />
-                    </div>
-                    <div className="container-fluid d-flex align-items-center justify-content-center">
-                      <span style={{ fontSize: "17px", fontWeight: "bold" }}>
-                        {company.name}
-                      </span>
-                    </div>
+                <div
+                  className="d-flex border-bottom border-main-light"
+                  style={{ minHeight: "110px" }}
+                >
+                  <div className="d-flex align-items-center">
+                    <img
+                      src={company.logo}
+                      style={{ maxWidth: "110px", maxHeight: "110px" }}
+                      alt={company.name}
+                    />
                   </div>
-                  <div className="card-body">
-                    <div
-                      className="card-text text-start"
-                      style={{ fontSize: "15px" }}
-                    >
-                      {company.min_employees && (
+                  <div className="container-fluid d-flex align-items-center justify-content-start ps-4">
+                    <span style={{ fontSize: "17px", fontWeight: "bold" }}>
+                      {company.name}
+                    </span>
+                  </div>
+                </div>
+                <div className="card-body">
+                  <div className="card-text text-start ts-smd">
+                    <div className="d-flex align-items-center gap-1">
+                      <IoMdPeople className="fs-5 text-main" />
+                      {company.min_employees ? (
                         <span>
-                          <span className="fw-bold">Quy mô:</span>
-                          {" " + company.min_employees}
+                          {company.min_employees}
                           {company.max_employees !== 0
                             ? " - " + company.max_employees
                             : "+ "}{" "}
                           nhân viên
-                          <br />
                         </span>
-                      )}
-                      <span className="text-multiline">
-                        <span className="fw-bold">Địa chỉ:</span>{" "}
-                        {company.address}
-                      </span>
-                      {company.website && (
-                        <span className="text-ellipsis">
-                          <span className="fw-bold">Website: </span>
-                          <a
-                            href={company.website}
-                            style={{ textDecoration: "none" }}
-                          >
-                            {company.website}
-                          </a>
-                        </span>
+                      ) : (
+                        "Chưa cập nhật"
                       )}
                     </div>
-                  </div>
-                  <div className="card-footer bg-white border-top-0 d-flex justify-content-center mb-1">
-                    <Link
-                      to={`/companies/${company.id}`}
-                      className="btn btn-sm btn-primary"
-                    >
-                      Chi tiết
-                    </Link>
+                    <div className="text-multiline">
+                      <MdLocationOn className="fs-5 text-main me-1" />
+                      {company.address}
+                    </div>
+                    {company.website && (
+                      <span className="text-ellipsis">
+                        <IoIosLink className="ts-lg text-main me-1" />
+                        <a
+                          href={company.website}
+                          className="hover-link text-secondary text-decoration-none"
+                        >
+                          {company.website}
+                        </a>
+                      </span>
+                    )}
                   </div>
                 </div>
               </div>
-            ))
-          ) : (
-            <h4 className="ms-3 text-start">Không có kết quả nào phù hợp!</h4>
-          )}
-        </div>
+            </div>
+          ))
+        ) : (
+          <h4 className="ms-3 text-start">Không có kết quả nào phù hợp!</h4>
+        )}
       </div>
       <div style={{ minHeight: "25px" }}></div>
-    </>
+    </div>
   );
 }
 
