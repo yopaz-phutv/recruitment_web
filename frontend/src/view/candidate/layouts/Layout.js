65222d24c276d57282350cac6d6e6716145f45db
tranphu <tranphudhak@gmail.com>
2024-03-10 14:35:18 +0700

    fix: change padding-bottom of layout

--- frontend/src/view/candidate/layouts/Layout.js
+++ frontend/src/view/candidate/layouts/Layout.js
@@ -242,7 +242,7 @@ function Layout(props) {
       </header>
       <main
         className="page-body"
-        style={{ marginTop: "57px", paddingBottom: '10px' }}
+        style={{ marginTop: "57px", paddingBottom: '20px' }}
       >
         {!isAuth && <Login />}
         {props.children}
